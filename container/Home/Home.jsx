eb9f5e9c8887185b9f30858731cc5aa34e48d941
astroboy20 <tolulopeakinkunmi7@gmail.com>
2023-12-05 21:48:32 +0100

    UPDATE

--- container/Home/Home.jsx
+++ container/Home/Home.jsx
@@ -9,7 +9,6 @@ import MultiStep from "react-multistep";
 const HomePage = () => {
   const [step, setStep] = useState(1);
   const router = useRouter();
- 
 
   const handleRoute = () => {
     router.push("/auth");
@@ -18,126 +17,147 @@ const HomePage = () => {
   const handleNext = () => {
     setStep((prevStep) => prevStep + 1);
   };
-  const handlePrev = () => {
-    setStep((prevStep) => prevStep - 1);
-  };
 
   return (
     <>
-        <>
-          {step === 1 && (
-            <>
-              <Container>
-                <StepOnestyle>
-                  <Image
-                    src={"/images/capture.png"}
-                    width={340}
-                    height={700}
-                    alt="."
-                  />
-                </StepOnestyle>
-                <div className="text">
-                  <CustomText weight={"500"} type={"Htype"} variant={"h2"}>
-                    Capture moments and memories on your big day
-                  </CustomText>
-                  <div>
-                    <Button
-                      type="submit"
-                      variant="defaultButton"
-                      onClick={handleNext}
-                    >
-                      Next
-                    </Button>
-                  </div>
+      <>
+        {step === 1 && (
+          <>
+            <Container>
+              <StepOnestyle>
+                <Image
+                  src={"/images/capture.png"}
+                  width={380}
+                  height={600}
+                  alt="."
+                />
+              </StepOnestyle>
+              <div className="text">
+                <CustomText weight={"500"} type={"Htype"} variant={"h2"}>
+                  Capture moments and memories on your big day
+                </CustomText>
+                <div
+                  style={{
+                    marginTop: "20px",
+                    fontSize: "16px",
+                    fontWeight: "800",
+                  }}
+                >
+                  <Button
+                    type="submit"
+                    variant="defaultButton"
+                    onClick={handleNext}
+                  >
+                    Next
+                  </Button>
                 </div>
-              </Container>
-            </>
-          )}
-          {step === 2 && (
-            <>
-              <Container>
-                <StepOnestyle>
-                  <Image
-                    src={"/images/several.svg"}
-                    width={340}
-                    height={700}
-                    alt="."
-                  />
-                </StepOnestyle>
-                <div className="text">
-                  <CustomText weight={"500"} type={"Htype"} variant={"h2"}>
-                    Take several cliqs with your created digital backdrops
-                  </CustomText>
-                  <div >
-                    <Button
-                      type="submit"
-                      variant="defaultButton"
-                      onClick={handleNext}
-                    >
-                      Next
-                    </Button>
-                  </div>
+              </div>
+            </Container>
+          </>
+        )}
+        {step === 2 && (
+          <>
+            <Container>
+              <StepOnestyle>
+                <Image
+                  src={"/images/several.svg"}
+                  width={380}
+                  height={600}
+                  alt="."
+                />
+              </StepOnestyle>
+              <div className="text">
+                <CustomText weight={"500"} type={"Htype"} variant={"h2"}>
+                  Take several cliqs with your created digital backdrops
+                </CustomText>
+                <div
+                  style={{
+                    marginTop: "20px",
+                    fontSize: "16px",
+                    fontWeight: "800",
+                  }}
+                >
+                  <Button
+                    type="submit"
+                    variant="defaultButton"
+                    onClick={handleNext}
+                  >
+                    Next
+                  </Button>
                 </div>
-              </Container>
-            </>
-          )}
-          {step === 3 && (
-            <>
-              <Container>
-                <StepOnestyle>
-                  <Image
-                    src={"/images/qr.svg"}
-                    width={400}
-                    height={700}
-                    alt="."
-                  />
-                </StepOnestyle>
-                <div className="text">
-                  <CustomText weight={"500"} type={"Htype"} variant={"h2"}>
-                    Guest scan barcode to access your digital backdrop
-                  </CustomText>
-                  <div >
-                    <Button
-                      type="submit"
-                      variant="defaultButton"
-                      onClick={handleNext}
-                    >
-                      Next
-                    </Button>
-                  </div>
+              </div>
+            </Container>
+          </>
+        )}
+        {step === 3 && (
+          <>
+            <Container>
+              <StepOnestyle>
+                <Image
+                  src={"/images/qr.svg"}
+                  width={380}
+                  height={600}
+                  alt="."
+                />
+              </StepOnestyle>
+              <div className="text">
+                <CustomText weight={"500"} type={"Htype"} variant={"h2"}>
+                  Guest scan barcode to access your digital backdrop
+                </CustomText>
+                <div
+                  style={{
+                    marginTop: "20px",
+                    fontSize: "16px",
+                    fontWeight: "800",
+                  }}
+                >
+                  <Button
+                    type="submit"
+                    variant="defaultButton"
+                    onClick={handleNext}
+                  >
+                    Next
+                  </Button>
                 </div>
-              </Container>
-            </>
-          )}
-          {step === 4 && (
-            <>
-              <Container>
-                <StepOnestyle>
-                  <Image
-                    src={"/images/album.svg"}
-                    width={400}
-                    height={700}
-                    alt="."
-                  />
-                </StepOnestyle>
-                <div className="text">
-                  <CustomText weight={"500"} type={"Htype"} variant={"h2"}>
-                    A digital album from cliqs taken at your event
-                  </CustomText>
-                  <div >
-                    <Button
-                      type="submit"
-                      variant="defaultButton"
-                      onClick={handleRoute}
-                    >
-                      Next
-                    </Button>
-                  </div>
+              </div>
+            </Container>
+          </>
+        )}
+        {step === 4 && (
+          <>
+            <Container>
+              <StepOnestyle>
+                <Image
+                  src={"/images/album.svg"}
+                  width={380}
+                  height={600}
+                  alt="."
+                />
+              </StepOnestyle>
+              <div className="text">
+                <CustomText weight={"500"} type={"Htype"} variant={"h2"}>
+                  A digital album from cliqs taken at your event
+                </CustomText>
+                <div
+                  style={{
+                    marginTop: "20px",
+                    fontSize: "16px",
+                    fontWeight: "800",
+                  }}
+                >
+                  <Button
+                    type="submit"
+                    variant="defaultButton"
+                    onClick={handleRoute}
+                  >
+                    Get Started
+                  </Button>
                 </div>
-              </Container>
-            </>
-          )}
-        </>
+              </div>
+            </Container>
+          </>
+        )}
+      </>
     </>
   );
 };
--- container/Home/Home.style.js
+++ container/Home/Home.style.js
@@ -7,11 +7,14 @@ export const Homestyle = styled.div`
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  text-align:center ;
+  gap:20px;
 `;
 export const StepOnestyle = styled.div`
   width: 100%; 
   .text {
     display: flex;
     flex-direction: column;
+    gap:20999px;
   }
 `;
