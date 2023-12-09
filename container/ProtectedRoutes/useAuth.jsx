4e5be51a86565029125c4a07c1f67e1d2af2d578
astroboy20 <tolulopeakinkunmi7@gmail.com>
2023-12-09 13:06:29 +0100

    update

--- container/ProtectedRoutes/useAuth.jsx
+++ container/ProtectedRoutes/useAuth.jsx
@@ -1,22 +1,18 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-
 const useAuth = () => {
   const { user } = useSelector((state) => state.auth);
   const googleToken = typeof window !== "undefined" && localStorage.getItem("token");
-  const [currentUser, setCurrentUser] = useState(null);
-
+  const [currentUser, setCurrentUser] = useState({});
+  console.log("hey",currentUser, user)
   useEffect(() => {
-    if (user) {
-      setCurrentUser(user);
-    } else if (googleToken) {
-      setCurrentUser({ token: googleToken }); 
+    if (user || googleToken) {
+      setCurrentUser({user}, googleToken);
     } else {
       setCurrentUser(null);
     }
   }, [user, googleToken]);
-
   return { currentUser };
 };
 
-export default useAuth;
+export  {useAuth};
