137394c79927835f4179264bd65f33fd66c3433b
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-04-22 12:20:34 +0100

    fixed error

--- feature/slices/authService.js
+++ feature/slices/authService.js
@@ -12,20 +12,12 @@ const NEW_PASS = "https://api-cliqpod.koyeb.app/auth/setNewPassword";
 //register user
 const register = async (userData) => {
   const response = await axios.post(REGISTER_URL, userData);
-  if (response.ok) {
-    localStorage.setItem("user", JSON.stringify(response.data));
-    return response.data.message;
-  }
   return response.data;
 };
 
 //register user
 const login = async (userData) => {
   const response = await axios.post(LOGIN_URL, userData);
-  if (response.data) {
-    localStorage.setItem("user", JSON.stringify(response.data));
-  }
-
   return response.data;
 };
 
