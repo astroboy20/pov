880dc48b5e3d3dcbfd7f6b910dcc92510f854859
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-04-24 22:07:07 +0100

    feat: finished the dashboard page

--- container/Dashboard/Dashboard.jsx
+++ container/Dashboard/Dashboard.jsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import styled from "./Dashboard.module.css";
 import { BottomNav } from "@/components/BottomNav";
 import { SearchIcon } from "@/assets";
+import Image from "next/image";
 
 const Dashboard = () => {
   const router = useRouter();
@@ -15,7 +16,69 @@ const Dashboard = () => {
         <div className="input">
           {" "}
           <SearchIcon />
-          <input type="text" placeholder="Search events here ..."/>
+          <input type="text" placeholder="Search events here ..." />
+        </div>
+      </div>
+      <div className="body">
+        <div className="upcoming-event">
+          <h1>Upcoming Events</h1>
+          <div className="scroll">
+            <div className="event-a">
+              <Image
+                src={"/images/wedding.svg"}
+                height={108}
+                width={100}
+                alt="wedding"
+              />
+              <div className="text">
+                <h3>The Quasar’24</h3>
+                <p>December 24, 2028.</p>
+                <p>Transcorp Hills, Ibadan.</p>
+                <p>Physical & Virtual</p>
+              </div>
+            </div>
+
+            <div className="event-b">
+              <Image
+                src={"/images/wedding.svg"}
+                height={108}
+                width={100}
+                alt="wedding"
+              />
+              <div className="text">
+                <h3>The Quasar’24</h3>
+                <p>December 24, 2028.</p>
+                <p>Transcorp Hills, Ibadan.</p>
+                <p>Physical & Virtual</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="popular-event">
+          <div className="event-header">
+            {" "}
+            <h1>Popular Events</h1> <p>View all</p>
+          </div>
+          <div className="event-body">
+            <div className="text"> <p>Sekani’s Bridal Party</p>
+            <span>View</span></div>
+           
+          </div>
+          <div className="event-body">
+            <div className="text"> <p>Sekani’s Bridal Party</p>
+            <span>View</span></div>
+           
+          </div>
+          <div className="event-body">
+            <div className="text"> <p>Sekani’s Bridal Party</p>
+            <span>View</span></div>
+           
+          </div>
+          <div className="event-body">
+            <div className="text"> <p>Sekani’s Bridal Party</p>
+            <span>View</span></div>
+           
+          </div>
         </div>
       </div>
     </DashboardStyle>
--- container/Dashboard/Dashboard.style.js
+++ container/Dashboard/Dashboard.style.js
@@ -4,25 +4,105 @@ import { MdDelete } from "react-icons/md";
 export const DashboardStyle = styled.div`
   display: flex;
   flex-direction: column;
-  overflow: scroll;
-  .header{
-    background:url("/images/header.svg") ;
-    height:200px ;
-    border-radius:0 0 15px 0 ;
-    padding:3% ;
+  .header {
+    background: url("/images/header.svg");
+    height: 200px;
+    border-radius: 0 0 15px 0;
+    padding: 3%;
   }
-  .input{
-    display:flex ;
-    gap:10px;
+  .input {
+    display: flex;
+    gap: 10px;
+    align-items: center;
+    background-color: #ffffff;
+    padding: 3%;
+    border-radius: 15px;
+    position: relative;
+    top: 130px;
+  }
+  .input input {
+    width: 100%;
+  }
+
+  .body {
+    padding: 6% 4%;
+    display: flex;
+    flex-direction: column;
+    gap: 60px;
+  }
+
+  .body .upcoming-event {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+  .body .scroll {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    width: 100%;
+    overflow-x: auto;
+  }
+
+  .event-a,
+  .event-b {
+    display: flex;
+    align-items: center;
+    box-shadow: 2px 2px 4px 0px #0000001a;
+    gap: 10px;
+    width: 90%;
+    flex-shrink: 0;
+  }
+  .upcoming-event .text h3 {
+    font-size: 16px;
+    font-weight: 700;
+    margin-bottom: 10px;
+  }
+  .upcoming-event p {
+    font-size: 14px;
+    font-weight: 700;
+  }
+  .upcoming-event h1,
+  .popular-event h1 {
+    font-size: 24px;
+    font-weight: 700;
+  }
+
+  .popular-event .event-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom:20px ;
+  }
+  .event-body {
+    background: url("/images/bg.svg");
+    height: 160px;
+    border-radius: 0 8px 8px 0;
+    padding:2% ;
+    margin-bottom:25px ;
+  }
+
+  .event-body .text {
+    display: flex;
+    justify-content: space-between;
     align-items:center ;
-    background-color:#FFFFFF ;
+    background: #f2f3f480;
+    border-radius: 8px;
     padding:3% ;
-    border-radius:15px ;
     position:relative ;
-    top:130px ;
+    top:100px;
   }
-  .input input{
-    width:100% ;
+  .event-body .text p {
+   font-size:16px ;
+   font-weight:700 ;
+  }
+  .event-body .text span {
+    background: #1D1465;
+    color:#fff ;
+    padding:5.5px 12px ;
+    border-radius:4px ;
+   font-size:16px ;
+   font-weight:700 ;
   }
 `;
 export const HeaderStyle = styled.div`
@@ -40,11 +120,11 @@ export const BodyStyle = styled.div`
   border-radius: 15px;
   text-align: left;
   padding: 5%;
-  gap:25px;
-  form{
-     display: flex;
-  flex-direction: column;
-  gap:20px;
+  gap: 25px;
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
   }
 `;
 export const FeatureStyle = styled.div`
@@ -60,8 +140,6 @@ export const FeatureStyle = styled.div`
   width: 100%;
   /* margin: 0 10%; */
 `;
-
-
 
 export const SettingStyle = styled.div`
   padding: 5%;
