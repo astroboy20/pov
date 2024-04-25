5269c11160e978d80dd74dcfe6d7f6ee48548364
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-04-25 09:13:02 +0100

    feat: update

---
+++ pages/customize.js
@@ -0,0 +1,10 @@
+import { Customize } from '@/container/Customize'
+import React from 'react'
+
+const customize = () => {
+  return (
+    <div className='body'><Customize/> </div>
+  )
+}
+
+export default customize
