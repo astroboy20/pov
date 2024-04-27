b694c0bb6808f1f23e8c14346de40c4723c3cb9c
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-04-27 15:48:06 +0100

    update

--- container/Invitee/Event/Event.jsx
+++ container/Invitee/Event/Event.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import { BlueBackIcon } from "@/assets";
 import { EventStyle } from "./Event.style";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { Button } from "@chakra-ui/react";
+import axios from "axios";
 
 const Event = () => {
   const router = useRouter();
-  const handleRoute = ()=>{
-    return router.push("/camera")
-  }
+  const handleRoute = () => {
+    return router.push("/camera");
+  };
+
+  const [events, setEvents] = useState([]); // Initialize as an array
+  const eventId = typeof window !== "undefined" && localStorage.getItem("id");
+  const { user } = useSelector((state) => state.auth);
+  const accessToken = user ? user.token : "";
+
+  useEffect(() => {
+    axios
+      .get(`https://api-cliqpod.koyeb.app/event/${eventId}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((response) => {
+        const data = response.data.event;
+        console.log(data);
+        setEvents(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching events:", error);
+      });
+  }, [eventId, accessToken]);
 
   return (
     <EventStyle>
